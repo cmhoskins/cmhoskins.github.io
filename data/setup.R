7e69c7909cf47ec784982e908e38f9bc23aa9e3e
Colleen <hoskinscolleen@gmail.com>
2024-12-09 16:32:39 -0700

    personal website

---
+++ data/setup.R
@@ -0,0 +1,40 @@
+#setup.R
+
+packageLoad <-
+  function(x) {
+    for (i in 1:length(x)) {
+      if (!x[i] %in% installed.packages()){
+        install.packages(x[i])
+      }
+      library((x[i]), character.only = TRUE)
+    }
+  }
+
+packageLoad('tidyverse')
+
+#create a string of package names
+packages <- c('tidyverse',
+              'palmerpenguins',
+              'rmarkdown',
+              'rstatix',
+              'purrr',
+              'lubridate',
+              'lterdatasampler',
+              'dplyr',
+              'ggplot2',
+              'plotly',
+              'dataRetrieval',
+              'httr',
+              'jsonlite',
+              'zoo',
+              'dataRetrieval',
+              'ggpubr',
+              'ggpmisc',
+              'sf',
+              'gridExtra')
+
+
+#use the packageLoad function we created on those packages
+packageLoad(packages)
+
+
